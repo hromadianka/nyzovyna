27765dc21eab335b0941655bc9367419c30ccd73
hromadianka <99715792+hromadianka@users.noreply.github.com>
2024-10-28 17:23:55 +0200

    add force default language middleware

--- anarchia/settings.py
+++ anarchia/settings.py
@@ -56,7 +56,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware'
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'app.middleware.ForceDefaultLanguageMiddleware'
 ]
 
 ROOT_URLCONF = 'anarchia.urls'
