66f5fe26066b364eb3a6a77bcfc58684b0815cc4
hromadianka <99715792+hromadianka@users.noreply.github.com>
2024-11-14 05:29:17 +0200

    Create feeds.py

---
+++ app/feeds.py
@@ -0,0 +1,22 @@
+from django.urls import reverse_lazy
+from django.contrib.syndication.views import Feed
+from .models import Article
+from django.utils.translation import get_language
+
+class LatestPostsFeed(Feed):
+    title = 'Nyzovyna'
+    link = reverse_lazy('home')
+    description = 'New articles on Nyzovyna.'
+
+    def items(self):
+        current_language = get_language()
+        return Article.objects.filter(language=current_language).order_by('-created_at')[:5]
+
+    def item_title(self, item):
+        return item.name
+
+    def item_description(self, item):
+        return item.text
+
+    def item_pubdate(self, item):
+        return item.created_at
