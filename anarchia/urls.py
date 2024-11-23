7c571dc6236d8b27d6ee04d751c2350f2a66b20e
hromadianka <99715792+hromadianka@users.noreply.github.com>
2024-11-23 18:00:19 +0200

    add ckeditor urls

--- anarchia/urls.py
+++ anarchia/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from app import views
 from django.conf.urls.static import static
 from django.conf import settings
@@ -25,6 +25,7 @@ from app.feeds import LatestPostsFeed
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('ckeditor/', include('ckeditor_uploader.urls')),
     path('set-language/', set_language, name='set_language'),
     path('', views.index, name='home'),
     path('about-us', views.about_us, name='about_us'),
