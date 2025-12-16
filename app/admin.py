ce53f32a751dd96b666afb59feb1fa972532c719
hromadianka <99715792+hromadianka@users.noreply.github.com>
2025-12-16 14:53:53 +0200

    Update comment view for admin panel

--- app/admin.py
+++ app/admin.py
@@ -5,6 +5,14 @@ from .models import AboutUsText, Category, Article, Comment, Author
 
 admin.site.register(Category)
 admin.site.register(Article)
-admin.site.register(Comment)
 admin.site.register(AboutUsText)
 admin.site.register(Author)
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('author', 'short_text', 'article', 'created_at', 'parent_comment')
+    list_select_related = ('article', 'parent_comment')
+
+    @admin.display(description='Текст')
+    def short_text(self, obj):
+        return obj.text[:50] + '…' if len(obj.text) > 50 else obj.text
